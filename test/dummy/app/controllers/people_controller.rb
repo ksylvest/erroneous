3740dba393245af6c5b3b961744af5525467f472
Kevin Sylvestre <kevin@ksylvest.com>
2011-10-22 23:23:32 -0700

    cleaned up project

--- test/dummy/app/controllers/people_controller.rb
+++ test/dummy/app/controllers/people_controller.rb
@@ -1,83 +1,49 @@
 class PeopleController < ApplicationController
+  
+  respond_to :html
+  
   # GET /people
-  # GET /people.json
   def index
     @people = Person.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @people }
-    end
+    respond_with(@people)
   end
 
-  # GET /people/1
-  # GET /people/1.json
+  # GET /people/:id
   def show
     @person = Person.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @person }
-    end
+    respond_with(@person)
   end
 
   # GET /people/new
-  # GET /people/new.json
   def new
     @person = Person.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @person }
-    end
+    respond_with(@person)
   end
 
-  # GET /people/1/edit
+  # GET /people/:id/edit
   def edit
     @person = Person.find(params[:id])
+    respond_with(@person)
   end
 
   # POST /people
-  # POST /people.json
   def create
-    @person = Person.new(params[:person])
-
-    respond_to do |format|
-      if @person.save
-        format.html { redirect_to @person, notice: 'Person was successfully created.' }
-        format.json { render json: @person, status: :created, location: @person }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @person.errors, status: :unprocessable_entity }
-      end
-    end
+    @person = Person.create(params[:person])
+    respond_with(@person)
   end
 
-  # PUT /people/1
-  # PUT /people/1.json
+  # PUT /people/:id
   def update
     @person = Person.find(params[:id])
-
-    respond_to do |format|
-      if @person.update_attributes(params[:person])
-        format.html { redirect_to @person, notice: 'Person was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @person.errors, status: :unprocessable_entity }
-      end
-    end
+    @person.attributes = params[:person]
+    @person.save
+    respond_with(@person)
   end
 
-  # DELETE /people/1
-  # DELETE /people/1.json
+  # DELETE /people/:id
   def destroy
     @person = Person.find(params[:id])
     @person.destroy
-
-    respond_to do |format|
-      format.html { redirect_to people_url }
-      format.json { head :ok }
-    end
+    respond_with(@person)
   end
 end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111023060417) do
+ActiveRecord::Schema.define(:version => 20110109192118) do
 
   create_table "people", :force => true do |t|
     t.string   "name"
